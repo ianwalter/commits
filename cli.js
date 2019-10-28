da846226161167b3697cd9da504461716fc51ec4
Ian Walter <public@iankwalter.com>
2019-10-28 13:04:05 -0400

    Adding #10

--- cli.js
+++ cli.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 
 const cli = require('@ianwalter/cli')
-const { print } = require('@ianwalter/print')
-const marked = require('marked')
-const TerminalRenderer = require('marked-terminal')
+const { print, md } = require('@ianwalter/print')
 const commits = require('.')
-
-marked.setOptions({ renderer: new TerminalRenderer() })
 
 async function run () {
   const { _: [start, end], merges } = cli({ name: 'commits' })
   const { description, markdown } = await commits(start, end, merges)
 
-  print.info(marked(description).trimEnd() + ':\n')
-
-  process.stdout.write(marked(markdown).trimEnd() + '\n')
+  print.info(md(description + ':'))
+  print.write('\n')
+  print.md(markdown)
+  print.write('\n')
 }
 
 run().catch(err => print.error(err))
